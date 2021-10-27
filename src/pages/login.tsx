f3b6ba55d16514a110ff81abd23253537a57aa5e
felipe <felipecostagomes@unipam.edu.br>
2021-10-27 19:28:25 -0300

    Created Login front

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,32 +1,56 @@
-import { Flex, Stack, Input, Button } from "@chakra-ui/react"
-import React, {useState} from "react"
-export default function Login (){
-    const [nome, setNome] = useState('')
-    const [senha, setSenha] = useState('')
-    async function handleLogin(){
-        const req = await fetch('',{
-            method: 'POST',
-            body: JSON.stringify({
-                nome,
-                senha
-            }),
-            headers:{
-                'Content-Type': 'application/json'
-            }
-        })
-        const json = await req.json()
+import {
+  Box,
+  Button,
+  Heading,
+  SimpleGrid,
+  Text,
+  useColorModeValue,
+  VisuallyHidden,
+} from '@chakra-ui/react'
+import * as React from 'react'
+import { FaFacebook, FaGithub, FaGoogle } from 'react-icons/fa'
+import { Card } from './../components/Login/Card'
+import { DividerWithText } from './../components/Login/DividerWithText'
+import { Link } from './../components/Login/Link'
+import { LoginForm } from './../components/Login/LoginForm'
+import { Logo } from './../components/Login/Logo'
 
-        console.log(json)
-    }
-    return(
-        <Flex w="100vw" h="100vh" align="center" justify="center" bg="black">
-            <Flex borderRadius="8" w="100%" maxW="360px" h="100%" maxH="500px" bg="gray.900" justify="center" align="center">
-                <Stack direction="column" spacing="6">
-                    <Input value={nome} onChange={e => setNome(e.target.value)} color="gray.50" placeholder="user" />
-                    <Input value={senha} onChange={e => setSenha(e.target.value)} type="password" color="gray.50" placeholder="senha" />
-                    <Button onClick={handleLogin} variant="solid" bg="blue.600" color="gray.50">Entrar</Button>
-                </Stack>
-            </Flex>
-        </Flex>
-    )
+export default function Login(){
+  return(
+<Box
+    bg={useColorModeValue('gray.50', 'inherit')}
+    minH="100vh"
+    py="12"
+    px={{ base: '4', lg: '8' }}
+  >
+    <Box maxW="md" mx="auto">
+      {/* <Logo mx="auto" h="8" mb={{ base: '10', md: '20' }} /> */}
+      <Heading textAlign="center" size="xl" fontWeight="extrabold">
+        Entre com sua conta de Organizador
+      </Heading>
+      <Text mt="4" mb="8" align="center" maxW="md" fontWeight="medium">
+        <Text as="span">Não possui uma conta?</Text>
+        <Link href="./Cadastro">Cadastre-se agora</Link>
+      </Text>
+      <Card>
+        <LoginForm />
+        <DividerWithText mt="6">ou faça login com</DividerWithText>
+        <SimpleGrid mt="6" columns={3} spacing="3">
+          <Button color="currentColor" variant="outline">
+            <VisuallyHidden>Login with Facebook</VisuallyHidden>
+            <FaFacebook />
+          </Button>
+          <Button color="currentColor" variant="outline">
+            <VisuallyHidden>Login with Google</VisuallyHidden>
+            <FaGoogle />
+          </Button>
+          <Button color="currentColor" variant="outline">
+            <VisuallyHidden>Login with Github</VisuallyHidden>
+            <FaGithub />
+          </Button>
+        </SimpleGrid>
+      </Card>
+    </Box>
+  </Box>
+  )
 }
